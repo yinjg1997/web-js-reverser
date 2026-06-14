dc66b32bc9759d3c30d91ddf2e80546ed0478dd6
yin <yinjg1997@gmail.com>
2026-06-14 21:28:33 +0800

    hook_function.js

---
+++ demo/hook/无限debugger/hook_function.js
@@ -0,0 +1,30 @@
+window = globalThis;
+(function () {
+    var _Function = window.Function
+    /**
+     * class 没有 arguments 和 caller 属性
+     * 箭头函数 没有 arguments 和 caller 属性
+     * function 有 arguments 和 caller 属性
+     */
+    window.Function = (code) => {
+        if (code.includes("debugger")) {
+            code = code.replaceAll("debugger", "")
+        }
+        return _Function.apply(window, code )
+    }
+    _Function.prototype.constructor = Function
+    Object.defineProperty(Function, "prototype", {
+        value: _Function.prototype,
+        writable: false,
+        enumerable: false,
+        configurable: true,
+    })
+
+    Object.defineProperty(Function, "length", {
+        value: 1,
+        writable: false,
+        enumerable: false,
+        configurable: true,
+    })
+})()
+
