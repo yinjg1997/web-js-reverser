9774e7ac30e0c2ee88c3da69fd00875ccfb2d159
yin <yinjg1997@gmail.com>
2026-06-16 16:48:51 +0800

    xhs

--- web逆向/小红书web/九州/test.js
+++ web逆向/小红书web/九州/test.js
@@ -3,12 +3,21 @@ const path = require('path');
 const vm = require('vm');
 
 const sandbox = {
+    // window: globalThis,
+    // globalThis: globalThis,
+    console: console,
+    performance: require('perf_hooks').performance,
+    TextEncoder: require('util').TextEncoder,
+    Event: function Event(){},
 };
 vm.createContext(sandbox);
 let code = ""
 for (const f of ['./x-s/env.js', './x-s/mnsv2.js']) {
-    code = code + "\n" + fs.readFileSync(path.join(__dirname, f), 'utf8');
+    // code = code + "\n" + fs.readFileSync(path.join(__dirname, f), 'utf8');
+    code = fs.readFileSync(path.join(__dirname, f), 'utf8');
+    vm.runInContext(code, sandbox);
 }
 // console.log(code)
-vm.runInContext(code, sandbox);
+// vm.runInContext(code, sandbox);
+// console.log(sandbox)
 console.log(sandbox.window.mnsv2)
